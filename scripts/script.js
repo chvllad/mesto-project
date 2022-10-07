213027958d18ba06d7733e734408323da714b409
Vladislav <112831869+chvllad@users.noreply.github.com>
2022-10-07 17:19:44 +0300

    Review fixes. Round 2.

--- scripts/script.js
+++ scripts/script.js
@@ -1,8 +1,21 @@
 const editEl = document.querySelector('.profile__edit');
+const nameEl = document.querySelector('.profile__name');
+const statusEl = document.querySelector('.profile__status');
+
 const closeEl = document.querySelector('.popup__close');
 const modalEl = document.querySelector('.popup');
 
+const editNameEl = document.querySelector('.profile-edit [name="name"]');
+const editStatusEl = document.querySelector('.profile-edit [name="status"]');
+const editFormEl = document.querySelector('.profile-edit');
+
+const likes = document.querySelectorAll('.place__like');
+const likedMap = new WeakMap();
+
 editEl.addEventListener('click', () => {
+  editNameEl.value = nameEl.textContent;
+  editStatusEl.value = statusEl.textContent;
+
   modalEl.classList.add('popup_opened');
 });
 
@@ -10,8 +23,14 @@ closeEl.addEventListener('mousedown', () => {
   modalEl.classList.remove('popup_opened');
 });
 
-const likes = document.querySelectorAll('.place__like');
-const likedMap = new WeakMap();
+editFormEl.addEventListener('submit', e => {
+  e.preventDefault();
+
+  nameEl.textContent = editNameEl.value;
+  statusEl.textContent = editStatusEl.value;
+
+  modalEl.classList.remove('popup_opened');
+})
 
 likes.forEach(like => {
   like.addEventListener('click', () => {
